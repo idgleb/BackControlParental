79af8aaa942f67b923b42e94df04333938be4f57
idgleb <idgleb646807@gmail.com>
2025-06-26 10:15:24 +0000

    Agrega meta CSRF token al formulario de registro

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Registro - Control Parental V2</title>
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     @if (app()->environment('local'))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
