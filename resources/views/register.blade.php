7bf60458d9f7ca52e9229ce88026119f170a05e2
idgleb <idgleb646807@gmail.com>
2025-06-26 12:06:36 +0000

    bd

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registro - Control Parental V2</title>
+    <title>Registro - Control Parental V4</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
